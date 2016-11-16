4139252123c6f7cc1c5d827c8922d4cdbfb6f126
adderbyte <adderbyte@users.noreply.github.com>
2016-11-16 16:07:36 +0100

    Update MyFunction

--- HomeWork_4/myFunction.py
+++ HomeWork_4/myFunction.py
@@ -2,6 +2,60 @@ from sklearn.preprocessing import LabelEncoder
 import numpy as np
 
 
+from sklearn.cross_validation import ShuffleSplit
+from sklearn.metrics import r2_score
+from collections import defaultdict
+from sklearn.ensemble import RandomForestClassifier
+
+
+ 
+def MeanDecreaseAccuracy(CrowdstormingDataJuly1st_Array,TargetVAriableRater,ShuffleSplitNumber,
+                         RandomState,CrowdstormingDataJuly1st):
+    '''
+    This gives the impact of each feature on the accuracy of the model. We permute the values of each 
+    feature and measure how much the permutation decreases the accuracy of the model. Permuting important 
+    variables should affect significantly the accuracy of model.
+    
+    Input: 
+    TargetVAriableRater as target 
+    CrowdstormingDataJuly1st_Array as Feature set 
+    
+    Output:
+    Features Sorted by their score with regard model accuracy
+    '''
+    for train_idx, test_idx in ShuffleSplit(ShuffleSplitNumber,RandomState):
+        X_train, X_test = CrowdstormingDataJuly1st_Array[train_idx], CrowdstormingDataJuly1st_Array[test_idx]
+        Y_train, Y_test = TargetVAriableRater[train_idx], TargetVAriableRater[test_idx]
+        X_train[np.isnan(X_train)]=0.0
+        X_test[np.isnan(X_test)]=0.0
+        Feature_name = CrowdstormingDataJuly1st.columns # Get Column Headings of our Dataframe
+        scores = defaultdict(list)
+
+        Y_train = Y_train[:,0]
+        Y_train = np.asarray(Y_train, dtype="|S6")
+        Y_test = Y_test[:,0]
+        type(Y_train)
+        type(Y_test)
+        Y_test=Y_test.astype(float)
+        ForestClassifier = RandomForestClassifier(n_estimators=100)
+        r = ForestClassifier.fit(X_train, Y_train)
+
+        tester = ForestClassifier.predict(X_test)
+        tester=tester.astype(np.float)
+
+        acc = r2_score(Y_test, tester)
+
+        for i in range(CrowdstormingDataJuly1st_Array.shape[1]):
+            X_t = X_test.copy()
+            np.random.shuffle(X_t[:, i])
+            tester2 = ForestClassifier.predict(X_t)
+            tester2=tester2.astype(np.float)
+            shuff_acc = r2_score(Y_test,tester2)
+            scores[Feature_name[i]].append((acc-shuff_acc)/acc)
+    ModelFeatureAccuracyScore = (sorted([(round(np.mean(score), 4), Feature_name) for
+                  Feature_name, score in scores.items()], reverse=True))
+    return ModelFeatureAccuracyScore
+
 class MultiColumnLabelEncoder(LabelEncoder):
     """
     Wraps sklearn LabelEncoder functionality for use on multiple columns of a
